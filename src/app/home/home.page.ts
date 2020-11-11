3d546964c39796d5ff7f9ac33f93d6ebadc4ae15
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-11-11 12:43:16 +0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,49 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+// How to create a variable
+	count = 0;
+	message = ""
+
+  constructor(public alertController: AlertController) {}
+
+  incrementPressed(){
+
+  	// to call global variable add this keyword
+  	// no need to add this for local variable
+
+  	this.count++
+  	this.message = ""
+  }
+  decrementPressed(){
+
+  	// if else same as in other language 
+  	// remember need to use this for global variable
+  	if (this.count == 0){
+  		this.message = "Number cannot be less than 0"
+
+  	}
+  	else {
+  		this.count--
+  	}
+  
+  }
+
+  resetPressed(){
+  	this.count = 0
+  	this.message = ""
+  }
+
+  async alertPressed(){
+  	 const alert = await this.alertController.create({
+      header: 'Hello World',
+      message: 'Welcome to my app',
+      buttons: ['Cancel', 'OK']
+    });
+
+    await alert.present();
+  }
+
+
 
 }
